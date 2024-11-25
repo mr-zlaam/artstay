fc85694b1ee2a167f2a22e2edf6ef752d3c16d8a
zlaam <zlaam.dev@gmail.com>
2024-11-26 03:45:47 +0500

    refactor: Update config file

--- src/configs/config.ts
+++ src/configs/config.ts
@@ -5,6 +5,6 @@ DotenvFlow.config();
 const config = {
   PORT: process.env.PORT || 8001,
   ENV: process.env.NODE_ENV as TENV,
-  JWT_SECRET: process.env.JWT_SECRET as string
+  JWT_SECRET: process.env.SECRET_KEY as string
 };
 export const { PORT, ENV, JWT_SECRET } = config;
