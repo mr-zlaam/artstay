20e124a50790b09847e90fe8cc3cd243512a5190
zlaam <zlaam.dev@gmail.com>
2024-11-29 08:20:49 +0500

    refactor: Move types file in dist

--- types/controllers/authController/authController.d.ts
+++
@@ -1,3 +0,0 @@
-export default class AuthController {
-  static register: (req: import("express").Request, res: import("express").Response, next: import("express").NextFunction) => void;
-}
--- types/controllers/performanceController/performanceController.d.ts
+++
@@ -1,4 +0,0 @@
-import type { Request, Response } from "express";
-export default class PerformanceController {
-  static getPerformance: (req: Request, res: Response) => void;
-}
