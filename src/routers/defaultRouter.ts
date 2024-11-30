0878a3e1715a99e0caea219b432a093307223d91
zlaam <zlaam.dev@gmail.com>
2024-11-30 22:22:27 +0500

    feat: Add more roles in model

--- src/routers/defaultRouter.ts
+++ src/routers/defaultRouter.ts
@@ -3,7 +3,9 @@ import { performanceRouter } from "./performanceRouter/performanceRouter.js";
 import { authRouter } from "./authRouter/authRouter.js";
 
 const defaultRouter: Router = Router();
+// ** Performance Routes **
 defaultRouter.use("/getPerformance", performanceRouter);
+// ** Auth Routes **
 defaultRouter.use("/auth", authRouter);
 
 export default defaultRouter;
